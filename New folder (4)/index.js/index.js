4ef94f184c2a294ba5c6c1f50887b99a4cdddc6d
abhay-081060 <76918911+abhay-081060@users.noreply.github.com>
2021-05-20 10:33:28 +0530

    Add files via upload
    just learning

---
+++ New folder (4)/index.js/index.js
@@ -0,0 +1,20 @@
+console.time("your code took");
+console.log('Hello word');
+console.log(4*34);
+console.log(true);
+console.log('chutiya');
+console.log([34,2,3,4,5,6,7]);
+console.timeEnd("your code took");
+var name="Abhay";
+var marks=43;
+var channel ="201";
+console.log(name,marks,channel);
+let name1 ='Abhay';
+console.log(name1);
+console.log("Data type is "+(typeof name1));
+function find() {
+    
+}
+console.log( (typeof find))
+let date = String(new Date());
+console.log(date,(typeof date))
